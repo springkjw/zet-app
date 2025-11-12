f66566336b7333d0a161fd1a4a23d106594447cd
권재원 <springkjw@gmail.com>
2025-11-12 23:34:26 +0900

    feat(icons): 톱니바퀴 아이콘 추가
    - 설정 관련 UI에 활용할 톱니바퀴 SVG 아이콘 컴포넌트 신규 추가
    - 아이콘 번들에서 `GearIcon` 내보내기

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as BellIcon } from "./BellIcon";
 export { default as ChevronDownIcon } from "./ChevronDownIcon";
 export { default as ChevronLeftIcon } from "./ChevronLeftIcon";
+export { default as GearIcon } from "./GearIcon";
 export { default as UserIcon } from "./UserIcon";
